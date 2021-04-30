c55eec1857942542b98efe215971ac6a7ad6fdcc
codeandcreateweb <codeandcreateweb@gmail.com>
2021-04-30 20:38:54 +1000

    Added more forecast data to be served

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,16 @@ const request = require('request')
 
 
 const forecast= (longitude, latitude, callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=9f2b54294d71b9e081c8e402751686b4&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=9f2b54294d71b9e081c8e402751686b4&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=m'
     request ({url, json:true},(error, { body }) =>{
         if(error){
             callback("unable to connect to weather services", undefined)
         }else if(body.error){
             callback("unable to to find location", undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0]+'. it is currently '+ body.current.temperature + " degrees out, feels like "+ body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0]+'. it is currently '+ 
+            body.current.temperature + " degrees, feels like "+ body.current.feelslike+ " degrees out, humidity: "+
+             body.current.humidity+" wind direction: "+ body.current.wind_dir +" wind speed: "+body.current.wind_speed)
         }
     })
 }
